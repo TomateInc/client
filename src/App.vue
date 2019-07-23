dd6a8c3bb8d6a501128ad5971b57c9f850992771
Colman Joeri <colmanjoeri@msn.com>
2019-07-23 15:34:00 +0200

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
-	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
-		<router-view />
-	</div>
+	<v-app id="inspire" dark>
+		<v-navigation-drawer v-model="drawer" clipped fixed app>
+			<v-list dense>
+				<v-list-tile>
+					<v-list-tile-action>
+						<v-icon>dashboard</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title><router-link to="/">Home</router-link></v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+				<v-list-tile>
+					<v-list-tile-action>
+						<v-icon>settings</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title><router-link to="/about">About</router-link></v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+		<v-toolbar app fixed clipped-left>
+			<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+			<v-toolbar-title>Application</v-toolbar-title>
+		</v-toolbar>
+		<v-content>
+			<router-view />
+		</v-content>
+		<v-footer app fixed>
+			<span>&copy; 2017</span>
+		</v-footer>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-#nav {
-	padding: 30px;
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
-}
-</style>
+<script>
+export default {
+	data: () => ({
+		drawer: null,
+	}),
+};
+</script>
