c0f3a964cfa2821c08b5c498d541dad05763a783
depuits <colmanjoeri@gmail.com>
2020-01-27 18:15:22 +0100

    implemented sensor ui

---
+++ src/devices/sensor.vue
@@ -0,0 +1,41 @@
+<template>
+	<v-card>
+		<v-card-title v-text="name"></v-card-title>
+
+		<v-card-subtitle>{{ display }}</v-card-subtitle>
+	</v-card>
+</template>
+
+<script>
+export default {
+	name: 'Sensor',
+	props: {
+		id: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		state: {
+			type: Object,
+			required: true,
+		},
+		config: {
+			type: Object,
+			required: true,
+		},
+	},
+	computed: {
+		display() {
+			let d = this.state.value;
+			if (d.toPrecision) {
+				d = d.toFixed(1);
+			}
+
+			return d + this.config.unit;
+		},
+	},
+};
+</script>
