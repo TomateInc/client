c6abb63996ad402beb0906ac2c4b142695ea407c
depuits <colmanjoeri@gmail.com>
2020-03-20 10:03:18 +0100

    fix for reloading devices after disconnect?

--- src/eventsource/index.js
+++ src/eventsource/index.js
@@ -1,7 +1,14 @@
 import Handlers from './handlers';
+import store from '@/store';
 
 export default function configureEventSources() {
 	const eventSource = new EventSource(`${process.env.VUE_APP_ROOT_API}/events`);
+
+	eventSource.onopen = (e) => {
+		console.log('The connection has been established.');
+		store.dispatch('loadDevices');
+	};
+
 	for (const handler of Handlers) {
 		eventSource.addEventListener(handler.eventType, event => {
 			handler.handle(event);
